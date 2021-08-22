cd01431b7f76671149201a5cee8cd9b332a6e6e2
katarzyna <rodakasia@wp.pl>
2021-08-22 07:54:36 +0200

    koekty formatowania

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let liczba = 12;
 
 if (liczba>10) {
-console.log("Podana liczba jest większa od 10");} else if(liczba<10) {console.log ("Podana liczba nie jest większa od 10");}
+    console.log("Podana liczba jest większa od 10");
+} else if(liczba<10) {
+    console.log("Podana liczba nie jest większa od 10");
+}
